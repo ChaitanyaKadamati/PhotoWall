72ca7169511defffcc6922fee8bffc8c8c527224
chaitanyakadamati <kskchaitanya@gmail.com>
2019-09-16 12:31:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosPresenterComponent } from './photos-presenter/photos-presenter.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AddPhotoComponent } from './add-photo/add-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosPresenterComponent,
+    WelcomeComponent,
+    AddPhotoComponent
   ],
   imports: [
     BrowserModule,
